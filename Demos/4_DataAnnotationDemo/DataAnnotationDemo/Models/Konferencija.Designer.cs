b48cd7329cd19512d8e0d2fcb488b1d5615c112c
Vedran Zdesic <vzdesic@gmail.com>
2016-01-03 16:36:39 +0100

    Ažuriranje prezentacija i backupa
    vzdesic

--- Demos/4_DataAnnotationDemo/DataAnnotationDemo/Models/Konferencija.Designer.cs
+++ Demos/4_DataAnnotationDemo/DataAnnotationDemo/Models/Konferencija.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Privatno\VERN\Programiranje za Internet\MVC\Demos\DataAnnotationDemo\DataAnnotationDemo\Models\Konferencija.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Privatno\VERN\Programiranje za Internet\MVC\GIT\Demos\4_DataAnnotationDemo\DataAnnotationDemo\Models\Konferencija.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
